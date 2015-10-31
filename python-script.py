b25cf21483d30ea3fed3460d8a2701320f312eab
LewisEllisWooster <he_wooster@ingham.org>
2015-10-31 12:30:07 -0400

    Update and rename python-script to python-script.py

---
+++ python-script.py
@@ -0,0 +1,23 @@
+###TINDER
+
+
+import pynder
+FBID = "1479365245705394"
+access_token="CAAGm0PX4ZCpsBAKc6gJBIZArWxAvZBsHBqm9VZA6xLHWfORFAnbP1kEpKWBp2xjBXUL013gLSgJQffj8Wo7p4FFk7ZC8iK3me2jZB2ZA4iyGGpD3xrLnoRXpLloXZAo9pFsXQKbIlIKJZBipZCAP8VxOcHDQQsdV4q6QAa15KB1ri1PorwuuCrOnxSEtkP94c9ZBKz2F46id5IOdQZDZD"
+session = pynder.Session(FBID,FACEBOOK_TOKEN)
+
+
+#################################3
+#spits JSON script
+with likes
+
+
+for usr in session.nearby_users():
+	usr.like()
+
+####################################
+
+
+#names of nearby users
+print session.nearby_users():
+
