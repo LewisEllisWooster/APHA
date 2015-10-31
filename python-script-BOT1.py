814eeabc25bc0f94f955866891ed6432855d2e93
LewisEllisWooster <he_wooster@ingham.org>
2015-10-31 13:05:57 -0400

    Update python-script-BOT1.py

--- python-script-BOT1.py
+++ python-script-BOT1.py
@@ -1,6 +1,10 @@
 ###TINDER
 
 
+
+#Session = BOT1
+
+
 import pynder
 FBID = "1479365245705394"
 access_token="CAAGm0PX4ZCpsBAKc6gJBIZArWxAvZBsHBqm9VZA6xLHWfORFAnbP1kEpKWBp2xjBXUL013gLSgJQffj8Wo7p4FFk7ZC8iK3me2jZB2ZA4iyGGpD3xrLnoRXpLloXZAo9pFsXQKbIlIKJZBipZCAP8VxOcHDQQsdV4q6QAa15KB1ri1PorwuuCrOnxSEtkP94c9ZBKz2F46id5IOdQZDZD"
@@ -24,6 +28,6 @@ print session.nearby_users():
 #gives profile name of bot usr
 session.profile
 
-#change location of usr
-session.update_location("23.9277432","2.7591924")
+#change location of usr to outback
+session.update_location("-23.3695078","126.5492293")
 
