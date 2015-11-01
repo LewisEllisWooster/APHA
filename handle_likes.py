8ae02941efc2bae627b87f5cfe82417357db43c6
LewisEllisWooster <he_wooster@ingham.org>
2015-11-01 10:54:19 -0500

    Update handle_likes.py

--- handle_likes.py
+++ handle_likes.py
@@ -3,14 +3,13 @@
 
 
 def handle_likes():
-    while True:
-        try:
-            users = session.nearby_users()
-            for u in users:
-                if u.name == 'Tinder Team':
-                    log('Out of swipes, pausing one hour...')
-                    return
-                u.like()
-                log('Liked ' + u.name)
-        except ValueError:
-            continue
+       
+    while BOT1.likes_remaining>0:
+	    try:
+	    	users = BOT1.nearby_users() #.nearby_users() object contains methods
+					    #for gender, age
+		    for u in users:
+			    u.like()
+			    log('Liked '+u.name())
+	    except ValueError:
+		    continue
